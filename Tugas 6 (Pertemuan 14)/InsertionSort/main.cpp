994bdb573e8e6b2b3cb08909a9cba0369f5f2e94
adeliaputriw <128129743+adeliaputriw@users.noreply.github.com>
2023-07-04 14:21:00 +0700

    Add files via upload

---
+++ Tugas 6 (Pertemuan 14)/InsertionSort/main.cpp
@@ -0,0 +1,44 @@
+/*
+    ADELIA PUTRI WIDYASARI
+    A11.2022.14426 | A11.4212
+    Dian Nuswantoro University
+    2023.06.26 Semarang
+*/
+
+#include <iostream>
+#include <chrono>
+using namespace std;
+using namespace std::chrono;
+
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int main() {
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    auto start = high_resolution_clock::now();
+    insertionSort(arr, n);
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    cout << "Time taken by insertion sort: " << duration.count() << " microseconds" << endl;
+
+    return 0;
+}
